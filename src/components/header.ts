67ddbfa2bf014cd7730c829414715cea6625abac
DiegoPertierraM <diegopmenendez@gmail.com>
2024-03-17 19:59:17 +0100

    Update to fix button behaviour and add pokemon counter

--- src/components/header.ts
+++ src/components/header.ts
@@ -11,7 +11,7 @@ export class Header extends Component {
   createTemplate() {
     return `
     <header class="header" role="heading" aria-level="1">
-      <h1 class="header__title"><img src="img/pokemon-logo.svg"></h1>
+      <h1 class="header__title"><a href="index.html"><img src="img/pokemon-logo.svg"></a></h1>
     </header>
     `;
   }
--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,13 @@ import { Footer } from './components/footer';
 import { Main } from './components/main';
 import { Menu } from './components/menu';
 import { Pokemon } from './components/pokemon';
+import { PokeList } from './components/poke-list';
+// import { Details } from './components/poke-details';
 
 new Header('body');
 new Menu('header');
 new Main('body');
 new Pokemon('main');
+new PokeList('.pokemon');
+// new Details('.pokemon');
 new Footer('body');
